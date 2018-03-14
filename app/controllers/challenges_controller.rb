21bb56b905fb9757cbbb4021e66f60fd8b7ac609
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-03-14 18:02:25 +0100

    other cases in btn

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -5,22 +5,22 @@ class ChallengesController < ApplicationController
     @commitment = Commitment.new
 
     # list all seen challenges
-    seen_challenges = []
+    @seen_challenges = []
     all_commitments = Commitment.where(user_id: current_user.id)
     all_commitments.each do |commitments|
-      seen_challenges << commitments.challenge
+      @seen_challenges << commitments.challenge
     end
     # check if a challenge is accepted
-    unseen_challenges = []
+    @unseen_challenges = []
     Challenge.all.each do |challenge|
     # if not, add it to unseen_challenge
-      unseen_challenges << challenge unless seen_challenges.include?(challenge)
+      @unseen_challenges << challenge unless @seen_challenges.include?(challenge)
     end
     # if a user has seen all challenges, pick a random one
-    if unseen_challenges == []
+    if @unseen_challenges == []
       @random_challenge = Challenge.all.sample
     else
-      @random_challenge = unseen_challenges.sample
+      @random_challenge = @unseen_challenges.sample
     end
   end
 
